e8a0dc2342e20646449e6e71b509e7ee1281e849
Patrick Little <patclittle@gmail.com>
2023-03-25 23:01:18 -0700

    fix

--- deployment/main/deploy.sh
+++ deployment/main/deploy.sh
@@ -1,2 +1,8 @@
 helm uninstall main
 helm install main .
+
+# sleep 1s to let the pod startup
+usleep 1000000
+
+echo "Go to http://localhost/ideas to try it out!"
+kubectl port-forward fern-project-main-68df48ccb4-ltmng 8080:8080
